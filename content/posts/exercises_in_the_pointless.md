3707894379cd6a2a60d1c9f8ae931e9dfb80eb37
theoboldalex <theoboldalex@gmail.com>
2023-05-20 13:54:04 +0100

    creates new post stub

---
+++ content/posts/exercises_in_the_pointless.md
@@ -0,0 +1,7 @@
+---
+title: "Exercises in the Pointless"
+date: 2023-05-20T13:51:57+01:00
+draft: true
+---
+
+Creating a static site generator with bash/awk etc.
--- main.py
+++
@@ -1,9 +0,0 @@
-def blast_off(num):
-    print(num)
-    if num == 0:
-        print('BLAST OFF!')
-        return
-
-    return blast_off(num - 1)
-
-blast_off(10)
